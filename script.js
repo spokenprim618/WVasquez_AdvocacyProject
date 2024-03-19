908d85a73f14e7bb16b3528efcd15f72330a621c
william vasquez <85375345+spokenprim618@users.noreply.github.com>
2024-03-19 05:19:07 +0000

    big

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let buttonData = document.querySelector("[data-butt]")
 let button = document.getElementById("resources")
-
+let filled = document.querySelector(".filled");
 button.addEventListener("click", function(){
   if(buttonData.dataset.butt === "false"){
     window.alert("You have not signed up for this feature")
@@ -8,3 +8,9 @@ button.addEventListener("click", function(){
     button.setAttribute("href","./re/Resources.html")
   }
 })
+
+function update(){
+  filled.style.width=`${((window.scrollY)/(document.body.scrollHeight-window.innerHeight)*100)}%`;
+  requestAnimationFrame(update);
+}
+update();
